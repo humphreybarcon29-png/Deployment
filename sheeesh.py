16fd44aa0fd62c456d6b63220fa71d5d524b9e6e
humphreybarcon29-png <humphreybarcon29@gmail.com>
2026-01-13 23:01:38 +0800

    Update sheeesh.py

--- sheeesh.py
+++ sheeesh.py
@@ -13,12 +13,7 @@ DB_NAME = os.environ.get('MYSQLDATABASE', 'chapter_renewal')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 class User(db.Model):
     __tablename__ = 'input'
@@ -27,9 +22,6 @@ class User(db.Model):
     Chapter_Company = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
 
 @app.route('/')
 def home():
@@ -40,35 +32,14 @@ def renewal():
     CO = request.form.get('CO')
     CC = request.form.get('CC')
     AIN = request.form.get('AIN')
-    PASSWORD = request.form.get('PS')
-
-    if not all([CO, CC, AIN, PASSWORD]):
-        flash('All fields required!', 'error')
-        return redirect(url_for('home'))
-
-    existing_user = User.query.filter_by(email=AIN).first()
-    if existing_user:
-        flash('Email already registered!', 'error')
-        return redirect(url_for('home'))
-
-    new_user = User(
-        Chapter_of_Origin=CO,
-        Chapter_Company=CC,
-        email=AIN,
-        password=PASSWORD
-    )
-    
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Renewal successful!', 'success')
-        return redirect(url_for('home'))
-    except Exception as e:
-        db.session.rollback()
-        flash(f'Error: {str(e)}', 'error')
-        return redirect(url_for('home'))
+    PS = request.form.get('PS')
+    if not all([CO, CC, AIN, PS]):
+        return "All fields required!", 400
+    user = User(Chapter_of_Origin=CO, Chapter_Company=CC, email=AIN, password=PS)
+    db.session.add(user)
+    db.session.commit()
+    return "Success!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=port)
